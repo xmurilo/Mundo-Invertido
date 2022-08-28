3827203558be67967794615fe1ad102023d9cdb2
xmurilo <107772420+xmurilo@users.noreply.github.com>
2022-08-28 00:22:15 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log('Passou por aqui');
+const txtName = document.getElementById('txtName')
+const txtEmail = document.getElementById ('txtEmail')
+const txtLevel = document.getElementById ('txtLevel')
+const txtCharacter = document.getElementById ('txtCharacter')
+const btnSubscribe = document.getElementById ('btnSubscribe')
+
+btnSubscribe.addEventListener('click', () => {
+  
+})
